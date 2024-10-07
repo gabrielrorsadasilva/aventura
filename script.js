2767aef0ff1a37a18027e2714c22da10cf8ddb78
gabrielrorsadasilva <biellflash2021@gmail.com>
2024-10-07 11:58:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const avance=documento.queryselecrorALL('btn_proximo')
+
+avance.array.forEach(button =>
+   botton.addEventisternum('click',function(){
+     const atual=document.querySelector('ativo');
+   cost proximoPasso= 'passo'-+ this.getAtribut('data-proxima')
+   
+   
+  atual.classList.remove('ativo');
+           document.getElementById(proximoPasso).classList.add('ativo');
+       })
+   })
+
+
+}
